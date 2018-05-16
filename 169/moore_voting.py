8a60eaeb0639a1d390123c46076ea6f08b05768a
yuandy <yuandychen@gmail.com>
2018-05-16 17:36:02 +0800

    first commit

---
+++ 169/moore_voting.py
@@ -0,0 +1,33 @@
+class Solution:
+    def majorityElement(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        maxIdx = 0
+        count = 1
+        # Moore's voting alg.
+        """
+        It's like we would cancel out each occurrence of an element e with all
+        the other element different from e so that it would exist till the end. 
+
+        Complexity: O(n)
+        Auxiliary Space: O(1)
+        """
+        for i in range(1, len(nums)):
+            if nums[i] == nums[maxIdx]:
+                count += 1
+            else:
+                count -= 1
+            if count == 0:
+                maxIdx = i
+                count = 1
+        # verify the majority element we found
+        occurrence = 0
+        for num in nums:
+            if num == nums[maxIdx]:
+                occurrence += 1
+        if occurrence > (len(nums) // 2):
+            return nums[maxIdx]
+        else:
+            return 0
---
+++ README.md
@@ -0,0 +1,5 @@
+#My Collections of Leetcode Problems
+This is a repository of my solutions to Leetcode problems.
+Implemented by Python3.6.
+Started from May 16.
+
