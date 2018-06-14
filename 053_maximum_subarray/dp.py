9a2456933b262913c3be31705fab3d1837e55053
yuandy <yuandychen@gmail.com>
2018-06-14 09:37:06 +0800

    add 053

---
+++ 053_maximum_subarray/dp.py
@@ -0,0 +1,20 @@
+class Solution:
+    def maxSubArray(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        """
+        Complexity: O(n)
+        Space: O(n)
+        let dp[i] be the maximum subarray ending at the ith index.
+        """
+        dp = [0]*len(nums)
+        dp[0] = nums[0]
+        max_value = dp[0]
+        for i in range(1, len(nums)):
+            dp[i] = dp[i-1] + nums[i] if dp[i-1] > 0 else nums[i]
+            max_value = max(max_value, dp[i])
+            
+        return max_value
+            
