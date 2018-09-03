9503f4baa8bc02e1773a6e300dc2b4021739d96f
yuandy <yuandychen@gmail.com>
2018-09-02 23:18:31 -0400

    add 003 comment

--- 003_longest_substring_without_replacing_characters/hash.cpp
+++ 003_longest_substring_without_replacing_characters/hash.cpp
@@ -6,6 +6,8 @@ public:
         std::unordered_map<char, int>used_char;
         // std::vector<int>used_char(256, -1)
         // if char using ASCII, could use this vector as the hashmap
+        // need to make sure start <= used_char[[s[i]]]
+        // since the idx used_char[s[i]] may be smaller than the start due to other repeating chars
         for(int i = 0; i<s.length(); ++i){
             if(used_char.find(s[i]) != used_char.end() && start <= used_char[s[i]]){
                 start = used_char[s[i]] + 1;
