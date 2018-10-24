c6b4008c124007b987688053054693d62fdd9aa3
yuandy <yuandychen@gmail.com>
2018-10-23 20:46:14 -0400

    add 766

---
+++ 766_Toeplitz_Matrix/try.cpp
@@ -0,0 +1,20 @@
+class Solution {
+  /*
+   * Complexity: O(m*n)
+   * Space: O(1)
+   */
+public:
+    bool isToeplitzMatrix(vector<vector<int>>& matrix) {
+        int m = matrix.size(), n = 0;
+        if(m){
+            n = matrix[0].size();
+            for(int i = 1; i < m; ++i){
+                for(int j = 1; j < n; ++j){
+                    if(matrix[i][j] != matrix[i-1][j-1]) return false;
+                }
+            }
+            return true;
+        }
+        return false;
+    }
+};
