79acf4d3be21e98bf87cb174758d0792370d7e5c
yuandy <yuandychen@gmail.com>
2018-11-06 22:05:04 -0500

    add 231

---
+++ 231_power_of_two/bit.cpp
@@ -0,0 +1,11 @@
+class Solution {
+  /*
+   * Complexit: O(1)
+   * Space: O(1)
+   */
+public:
+    bool isPowerOfTwo(int n) {
+        if(n <= 0) return false;
+        return (n & (n - 1)) == 0;
+    }
+};
---
+++ 231_power_of_two/try.cpp
@@ -0,0 +1,17 @@
+class Solution {
+  /*
+   * Complexity: O(lgn)  
+   * Space: O(1)
+   */
+public:
+    bool isPowerOfTwo(int n) {
+        if(n <= 0) return false;
+        if(n == 1) return true;
+        
+        while(n != 1){
+            if(n % 2 != 0) return false; 
+            n /= 2;
+        }
+        return true;
+    }
+};
